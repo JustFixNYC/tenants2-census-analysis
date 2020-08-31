4e9de93addf57152e2c309f7f5010bae2432a4b8
Atul Varma <varmaa@gmail.com>
2020-08-31 08:56:18 -0400

    Factor out HOUSEHOLDS_MEDIAN_INCOME_DOLLARS.

--- download_census_data.py
+++ download_census_data.py
@@ -10,15 +10,17 @@ CENSUS_API_KEY = os.environ['CENSUS_API_KEY']
 # https://www.census.gov/prod/techdoc/cbp/cbp95/st-cnty.pdf
 BRONX_COUNTY = '005'
 
+# These column names can be found from
+# the "ACS Subject Tables Variables", or you can find the
+# dataset you want at https://data.census.gov/ and download
+# the table that contains your data; it will contain a CSV
+# with the variable names you want, among other things.
+HOUSEHOLDS_MEDIAN_INCOME_DOLLARS = 'S1901_C01_012E'
+
 c = census.Census(CENSUS_API_KEY, year=2017)
 
 result = c.acs5st.get(
-    # These column names can be found from
-    # the "ACS Subject Tables Variables", or you can find the
-    # dataset you want at https://data.census.gov/ and download
-    # the table that contains your data; it will contain a CSV
-    # with the variable names you want, among other things.
-    ('NAME', 'S1901_C01_012E',),
+    ('NAME', HOUSEHOLDS_MEDIAN_INCOME_DOLLARS,),
     {
         'for': 'tract:*',
         'in': f'state:{states.NY.fips} county:{BRONX_COUNTY}',
